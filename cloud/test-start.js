ad4f043894f7e3498ed6ff26693220bab1a7ff5b
Tuyo Isaza <tboard@hotmail.com>
2026-03-12 08:15:48 -0500

    [16] fix: use npm ci with package-lock.json for consistent native modules

--- cloud/test-start.js
+++
@@ -1,9 +0,0 @@
-console.log('[TEST] Simple test script running...');
-console.log('[TEST] Node version:', process.version);
-console.log('[TEST] Current dir:', process.cwd());
-
-try {
-  require('./dist/main');
-} catch (e) {
-  console.error('[TEST] Error loading main:', e.message);
-}
